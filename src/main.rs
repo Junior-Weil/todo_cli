cc8d53e8bb073b063e45d30fd8197e3396de2157
Junior-Weil <rmw020@bucknell.edu>
2025-09-04 16:30:01 -0500

    starting project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufReader, Write};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Task {
+    description: String, // What the task is
+    done: bool,          // Whether it is complete
 }
+
+fn main() {
+    // Load tasks from file if exists, otherwise store in empty vec
+    let mut task = load_task();
+
+    // main loop, keep showing the menu until a user chooses to exit
+}
+
